1841a2710b72dcd3177c09a08f73e43b8cf2f21d
mikhawa <michaeljpitz@gmail.com>
2024-09-29 11:26:07 +0200

    thks Kevin

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -6,21 +6,27 @@ use App\Entity\Post;
 use App\Entity\Section;
 use App\Entity\Tag;
 use App\Entity\User;
-use App\Form\TagAutocompleteField;
+use App\Repository\TagRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PostType extends AbstractType
 {
+    private array $brutData;
+
+    public function __construct(
+        private TagRepository $tagRepository
+    ) {
+        $this->brutData = [];
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-
-
-
         $builder
             ->add('postTitle')
             ->add('postDescription')
@@ -50,7 +56,34 @@ class PostType extends AbstractType
                 'class' => User::class,
                 'choice_label' => 'username',
             ])
+            ->addEventListener(FormEvents::PRE_SUBMIT, $this->preSubmit(...))
+            ->addEventListener(FormEvents::SUBMIT, $this->submit(...));
         ;
+    }
+
+    private function preSubmit(FormEvent $event): void {
+        $data = $event->getData();
+        if (isset($data['tags']) && is_array($data['tags'])) {
+            $this->brutData = $data['tags'];
+            $data['tags'] = array_filter($data['tags'], 'ctype_digit');
+            $event->setData($data);
+        }
+    }
+
+    private function submit(FormEvent $event): void {
+        $form = $event->getForm();
+        /** @var Post $post */
+        $post = $form->getData();
+        $data = $this->brutData;
+        foreach ($data as $tagName) {
+            if(ctype_digit($tagName)) continue;
+            if($this->tagRepository->findOneBy([
+                    'tagName' => $tagName
+                ]) !== null) continue;
+            $tag = new Tag();
+            $tag->setTagName($tagName);
+            $post->addTag($tag);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
